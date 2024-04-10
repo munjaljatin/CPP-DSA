d84bc0553bba374d577d675ace1cabe0db92fcf6
Jatin Munjal <munjal.jatin24@gmail.com>
2024-04-10 19:03:40 +0530

    Essentials section updated

--- Essentials/arithmeticTemplateClass.cpp
+++ Essentials/arithmeticTemplateClass.cpp
@@ -2,51 +2,33 @@
 using namespace std;
 
 // Generic Template for class Arithmetic
-template <class T>
-class Arithmetic
-{
+template <class T> class Arithmetic {
 private:
-    T a;
-    T b;
+  T a;
+  T b;
 
 public:
-    Arithmetic()
-    {
-        a = 0;
-        b = 0;
-    }
-    Arithmetic(T a, T b);
-    T add();
-    T subtract();
-    T multiplication();
+  Arithmetic() {
+    a = 0;
+    b = 0;
+  }
+  Arithmetic(T a, T b);
+  T add();
+  T subtract();
+  T multiplication();
 };
 
-template <class T>
-Arithmetic<T>::Arithmetic(T a, T b)
-{
-    this->a = a;
-    this->b = b;
+template <class T> Arithmetic<T>::Arithmetic(T a, T b) {
+  this->a = a;
+  this->b = b;
 }
 
-template <class T>
-T Arithmetic<T>::add()
-{
-    return (a + b);
-}
-template <class T>
-T Arithmetic<T>::subtract()
-{
-    return (a - b);
-}
+template <class T> T Arithmetic<T>::add() { return (a + b); }
+template <class T> T Arithmetic<T>::subtract() { return (a - b); }
 
-template <class T>
-T Arithmetic<T>::multiplication()
-{
-    return (a * b);
-}
+template <class T> T Arithmetic<T>::multiplication() { return (a * b); }
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    return 0;
+int main(int argc, char const *argv[]) {
+  /* code */
+  return 0;
 }
--- Essentials/rectangleClass.cpp
+++ Essentials/rectangleClass.cpp
@@ -1,58 +1,43 @@
 #include <iostream>
 using namespace std;
 
-class Rectangle
-{
+class Rectangle {
 private:
-    int length;
-    int breadth;
+  int length;
+  int breadth;
 
 public:
-    // Constructor function for initialising the object of type Rectangle
-    Rectangle(int l, int b)
-    {
-        length = l;
-        breadth = b;
-    }
-    // area function for calculating the area of rectangle
-    int area()
-    {
-        return length * breadth;
-    }
-    // function for calculating the perimeter of rectangle
-    int perimeter()
-    {
-        return (2 * (length + breadth));
-    }
-    // function for changing the lenght of rectangle
-    void changeLength(int l)
-    {
-        length = l;
-    }
-    // function for changing the breadth of reactangle
-    void changeBreadth(int b)
-    {
-        breadth = b;
-    }
-    // function for changing the length & breadth simultaneously
-    void changeLB(int l, int b)
-    {
-        length = l;
-        breadth = b;
-    }
+  // Constructor function for initialising the object of type Rectangle
+  Rectangle(int l, int b) {
+    length = l;
+    breadth = b;
+  }
+  // area function for calculating the area of rectangle
+  int area() { return length * breadth; }
+  // function for calculating the perimeter of rectangle
+  int perimeter() { return (2 * (length + breadth)); }
+  // function for changing the lenght of rectangle
+  void changeLength(int l) { length = l; }
+  // function for changing the breadth of reactangle
+  void changeBreadth(int b) { breadth = b; }
+  // function for changing the length & breadth simultaneously
+  void changeLB(int l, int b) {
+    length = l;
+    breadth = b;
+  }
 };
 
-int main(int argc, char const *argv[])
-{
-    /* code */
-    Rectangle r1(20, 30);
-    int area = r1.area();
-    cout << "Area of Rectangle = " << area << "\n";
-    int perimeter = r1.perimeter();
-    cout << "Perimeter of Rectangle = " << perimeter << '\n';
-    r1.changeLB(45, 55);
-    cout << "Area & Perimeter of Rectangle after changing the length and breadth" << '\n';
-    cout << "Area = " << r1.area() << " & "
-         << "Perimeter = " << r1.perimeter();
-    return 0;
+int main(int argc, char const *argv[]) {
+  /* code */
+  Rectangle r1(20, 30);
+  int area = r1.area();
+  cout << "Area of Rectangle = " << area << "\n";
+  int perimeter = r1.perimeter();
+  cout << "Perimeter of Rectangle = " << perimeter << '\n';
+  r1.changeLB(45, 55);
+  cout << "Area & Perimeter of Rectangle after changing the length and breadth"
+       << '\n';
+  cout << "Area = " << r1.area() << " & "
+       << "Perimeter = " << r1.perimeter();
+  return 0;
 }
